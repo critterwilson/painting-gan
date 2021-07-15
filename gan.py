9a6f0a379fc5794787c6148e255612c6516e2bd2
critterwilson <christopherwilsontry@gmail.com>
2021-07-15 08:24:17 -0600

    Adding resources

---
+++ gan.py
@@ -0,0 +1,22 @@
+# 
+
+# imports
+import os
+import tensorflow as tf
+import numpy as np
+import cv2
+import scipy.misc
+from utils import *
+
+
+# generator
+
+
+# discriminator
+
+
+# GAN1 - Photo to Monet
+
+# GAN2 - Monet to Photo
+
+
