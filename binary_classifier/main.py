358ff0743381d88b4de302ac034547ee641f6c94
critterwilson <christopherwilsontry@gmail.com>
2021-07-23 08:29:31 -0600

    My first attempt

--- binary_classifier/main.py
+++ binary_classifier/main.py
@@ -1,11 +1,16 @@
 import os
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam # only optimizer I could find for binary classification
 from torchvision.io import read_image
 from torch.utils.data import Dataset, DataLoader
 
 BATCH_SIZE = 1
+LEARNING_RATE = 0.001
+EPOCHS = 5
 
-
+# ???
 class ImageDataset(Dataset):
     def __init__(self, dirname):
         super(Dataset).__init__()
@@ -24,7 +29,6 @@ class ImageDataset(Dataset):
     def __len__(self):
         return len(self.images)
 
-
 train_dataset = ImageDataset("./data/train")
 validation_dataset = ImageDataset("./data/validate")
 test_dataset = ImageDataset("./data/test")
@@ -33,7 +37,37 @@ train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True
 validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE)
 test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
+class BinaryClassifier(nn.Module):
+    def __init__(self):
+        super(BinaryClassifier, self).__init__()
+        self.layer_1 = nn.Linear(in_features=256*256*3, out_features=1) #???
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = F.relu(x)
+        output = F.log_softmax(x, dim=1) #??? dim????
+        return output
+
+def train(model, optimizer):
+    for epoch in range(EPOCHS):
+        for batch_number, (image, label) in enumerate(train_dataloader):
+            image = image.view(-1, 256*256*3).to(torch.float32)
+            prediction = model(image)
+            loss = F.binary_cross_entropy(prediction.to(torch.float32, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step
+            
+            if batch_number % 10 == 0:
+                print(f"Epoch: {epoch} \t |Batch: {batch_number} \t | Loss: {loss}")
+
 if __name__ == "__main__":
-    for batch_number, (image, label) in enumerate(train_dataloader):
-        print(image, label)
-        break
+    model = BinaryClassifier()
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train(model, optimizer)
+    # for batch_number, (image, label) in enumerate(train_dataloader):
+    #     print(image.size()) # view???? -> turns tensor into different shape
+    #     break
+
+
--- binary_classifier/prepare_data.py
+++ binary_classifier/prepare_data.py
@@ -26,26 +26,26 @@ def copy_files(files: List[str], dirname: str, stage_dir: str, file_type: str) -
 
 def split_and_move(dirname: str) -> None:
 
-    id = dirname.split("/")[-1].split("_")[0]
+    id = dirname.split("/")[-1].split("_")[0] # splitting gan-getting-started/monet_jpg -> monet
 
-    files = os.listdir(dirname)
+    files = os.listdir(dirname) 
 
     num_files = len(files)
 
-    np.random.seed(0)
-    train, validate, test = np.split(files, [int(num_files * 0.7), int(num_files * 0.9)])
-
-    data_dir = os.path.join(os.path.dirname(HERE), "data")
+    np.random.seed(0) # creates a rand numpy array?
+    train, validate, test = np.split(files, [int(num_files * 0.7), int(num_files * 0.9)]) # splits the data into train/test/validate sets
+                                                                                          # what are the parameters?
+    data_dir = os.path.join(os.path.dirname(HERE), "data") # creates a new directory to store our data
     check_and_create(data_dir)
 
-    train_dir = os.path.join(data_dir, "train")
-    validate_dir = os.path.join(data_dir, "validate")
-    test_dir = os.path.join(data_dir, "test")
+    train_dir = os.path.join(data_dir, "train") # creates sub directory for train
+    validate_dir = os.path.join(data_dir, "validate") # " " for val
+    test_dir = os.path.join(data_dir, "test") # " " for test
 
-    stage_dirs = (train_dir, validate_dir, test_dir)
-    stage_files = (train, validate, test)
+    stage_dirs = (train_dir, validate_dir, test_dir) # tuple of our directory names
+    stage_files = (train, validate, test) # tuple of our filenames
 
-    for d, f in zip(stage_dirs, stage_files):
+    for d, f in zip(stage_dirs, stage_files): # copy the file over
         check_and_create(d)
         copy_files(f, dirname, d, id)
 
